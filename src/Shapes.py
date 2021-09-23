cf3b186cf857335a14d786ea5e25353916043778
EramKafeel <86064431+EramKafeel@users.noreply.github.com>
2021-09-23 16:58:30 +0530

    Add files via upload

---
+++ src/Shapes.py
@@ -0,0 +1,34 @@
+#right(angle)
+from turtle import *
+forward(100)
+right(90)
+forward(100)
+
+#left(angle)
+from turtle import *
+forward(100)
+left(90)
+forward(100)
+
+#Square
+from turtle import *
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+#Triangle
+from turtle import *
+forward (174)
+right (120)
+forward (174)
+right (120)
+forward (174)
+
+#circle
+import turtle
+t = turtle.Turtle()
+t.circle(50)
